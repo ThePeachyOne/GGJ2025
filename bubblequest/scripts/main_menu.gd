222d3a16a90ab5311fa92a1f9d00452d7f6088ea
Fox Lord <supremefoxlord@gmail.com>
2025-01-26 16:16:20 -0500

    Credits Button on Main Menu

--- bubblequest/scripts/main_menu.gd
+++ bubblequest/scripts/main_menu.gd
@@ -19,5 +19,5 @@ func _on_quit_pressed():
 	get_tree().quit()
 
 
-func _on_options_pressed():
-	pass # Replace with function body.
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
